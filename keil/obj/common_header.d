7c29ea512aedbae79756cea74015c66c9f87e653
Yang Zhou <gavinzhou10c@gmail.com>
2014-04-08 14:32:38 -0400

    Start to combine the project

--- keil/obj/common_header.d
+++
@@ -1,3 +0,0 @@
-.\obj\common_header.o: ..\common_header.c
-.\obj\common_header.o: C:\Program Files\Keil\ARM\ARMCC\bin\..\include\stdlib.h
-.\obj\common_header.o: C:\Program Files\Keil\ARM\ARMCC\bin\..\include\stdio.h
--- keypad.h
+++
@@ -1,8 +0,0 @@
-#ifndef KEYPAD_H
-#define KEYPAD_H
-
-#include "stm32f4xx.h"
-
-void keypad_init(void);
-uint8_t readKeys(void);
-#endif
