5b872bf5b0bdb8fddddd8aef60234bf8a9a04521
Yang Zhou <gavinzhou10c@gmail.com>
2014-04-09 02:44:03 -0400

    Create semaphore in the main function

--- TX/rtx_project/main.c
+++ TX/rtx_project/main.c
@@ -6,7 +6,6 @@
 #include "wireless_Impl.h"
 #include "tilt_thread.h"
 
-
 #include "keypad.h"
 #include "stdio.h"
 #include <math.h>
@@ -15,44 +14,36 @@
 #include "userbutton_config.h"
 
 
-
-
-int32_t key_signal      = 3;
+int32_t key_signal = 3;
+// Function prototype
 void thread_keypad(void const * argument);
-
-
-
-
-
-
-
-
-
-
-
-//! Thread def for above thread
-//osThreadDef(temperature_detection_thread, osPriorityNormal, 1, 0); // temperature detection thread
-osThreadDef(tilt_detection_thread, osPriorityNormal, 1, 0);        // tilt angles detection thread
-osThreadDef(transmitTiltAngles, osPriorityNormal, 1, 0);             // tilt transmit thread
-//osThreadDef(transmitTiltAngles, osPriorityNormal, 1, 0);             // tilt transmit thread
-
-// thread ids
+void stop_all_threads(void);
+void test_thread_function(void const * argument);
+// Thread ids
 osThreadId temperature_TurnDispFlag_thread;
 osThreadId tilt_TurnDispFlag_thread;
 osThreadId Alarm_LED_threadId;
 osThreadId tilt_transmit_thread;
-
-
-
-
-
-
-osThreadDef(thread_keypad, osPriorityNormal, 1, 0);
 osThreadId keypad_thread;
+osThreadId test_thread_id;
+
+// Thread defination
+//osThreadDef(temperature_detection_thread, osPriorityNormal, 1, 0); // temperature detection thread
+osThreadDef(tilt_detection_thread, osPriorityNormal, 1, 0);        // tilt angles detection thread
+osThreadDef(transmitTiltAngles, osPriorityNormal, 1, 0);             // tilt transmit thread
+osThreadDef(thread_keypad, osPriorityNormal, 1, 0);
+//osThreadDef(transmitTiltAngles, osPriorityNormal, 1, 0);             // tilt transmit thread
+osThreadDef(test_thread_function, osPriorityNormal, 1, 0);
+
+// Semaphore creation
+osSemaphoreId semaphore;
+osSemaphoreDef(semaphore);
 
 
 
-
+// Thread status which will be used in the thread termination
+osStatus keypad_thread_status;
+osStatus test_thread_status;
 
 
 /*!
@@ -68,23 +59,13 @@ int main (void) {
 	osThreadId tid_thread1;
 	GPIO_InitTypeDef  GPIO_InitStructure;
 
-
-  tilt_thread = osThreadCreate(osThread(tilt_detection_thread),NULL);
+//Make thread active 
+//  tilt_thread = osThreadCreate(osThread(tilt_detection_thread),NULL);
 //	tilt_transmit_thread = osThreadCreate(osThread(transmitTiltAngles),NULL);
-	
-	
-	
-	
-	
-	
-keypad_thread = osThreadCreate(osThread(thread_keypad), NULL);
-	
-	
-	
-	
-	
-	
-	
+//	keypad_thread = osThreadCreate(osThread(thread_keypad), NULL);
+	test_thread_id = osThreadCreate(osThread(test_thread_function), NULL);
+	osDelay(1000);
+	test_thread_status = osThreadTerminate(test_thread_id);
 	wireless_spi_Init();
 	
 	//----------------TX------------------------------
@@ -93,14 +74,6 @@ keypad_thread = osThreadCreate(osThread(thread_keypad), NULL);
 	readreg = read_Status_Register(WIRELESS_STATUS_VERSION);
 	
 }
-
-void thread (void const *argument) {
-
-}
-
-
-
-
 
 
 void thread_keypad(void const *argument){
@@ -158,13 +131,12 @@ void EXTI0_IRQHandler(void)
 		// call the push button interrupt isr
 		//PushButton_ISR();
 		printf("botton pressed. \n");
+		stop_all_threads();
 //	  osThreadTerminate (tilt_transmit_thread);
 //		osThreadTerminate (tilt_TurnDispFlag_thread);
-		osThreadTerminate(keypad_thread);
+		
 		// wait to avoid button debounce
 		//LCD_Delay_Longer(100);
-
-		
 
     /* Clear the EXTI line 1 pending bit */
     EXTI_ClearITPendingBit(EXTI_Line0);
@@ -172,6 +144,17 @@ void EXTI0_IRQHandler(void)
   }
 }
 
+void stop_all_threads(void){
+	keypad_thread_status = osThreadTerminate(keypad_thread);
+	test_thread_status = osThreadTerminate(test_thread_id);
+	return;
+}
 
+void test_thread_function(void const * argument){
+	while(1){
+		printf("This is test thread \n");
+		osDelay(500);
+	}
+}
 
 
